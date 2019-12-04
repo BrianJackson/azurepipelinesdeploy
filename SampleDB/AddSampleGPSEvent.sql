164c918993a0fd04dfaf7c70c349d8d571eb46df
Brian Jackson <brianjac@microsoft.com>
2019-12-04 02:17:49 -0500

    Fixed several issues in arm and yaml

---
+++ SampleDB/AddSampleGPSEvent.sql
@@ -0,0 +1,46 @@
+﻿CREATE PROCEDURE [dbo].[AddSampleGPSEvent]
+@VehicleId varchar(50)
+AS
+DECLARE @EventDate as smalldatetime
+
+SET @EventDate = GETDATE();
+
+/* Insert a sample row based on an NYC MTA bus event */	
+INSERT INTO [dbo].[GPSEvents]
+           ([RecordedAtTime]
+           ,[DirectionRef]
+           ,[PublishedLineName]
+           ,[OriginName]
+           ,[OriginLat]
+           ,[OriginLong]
+           ,[DestinationName]
+           ,[DestinationLat]
+           ,[DestinationLong]
+           ,[VehicleRef]
+           ,[VehicleLocationLat]
+           ,[VehicleLocationLong]
+           ,[NextStopPointName]
+           ,[ArrivalProximityText]
+           ,[DistanceFromStop]
+           ,[ExpectedArrivalTime]
+           ,[ScheduledArrivalTime])
+     VALUES
+           (@EventDate
+           ,DATEPART(s, @EventDate) % 2
+           ,'B8'
+           , '4 AV/95 ST'
+           , 40.616104
+           ,-74.031143
+           ,'BROWNSVILLE ROCKAWAY AV'
+           ,40.656048
+           ,-73.907379
+           ,@VehicleId
+           ,40.63517
+           ,-73.960803
+           ,'FOSTER AV/E 18 ST'
+           ,'approaching'
+           ,76
+           ,DATEADD(mi,(DATEPART(s, @EventDate) % 5), @EventDate)
+           ,DATEADD(mi,(DATEPART(s, @EventDate) % 5), @EventDate) - DATEPART(s, @EventDate) % 2
+		   )
+RETURN 
